d0b0a0d9f9ba20f9e45913807cee95c8a5a198fb
dineshkm123 <dineshkmg2003@gmail.com>
2024-02-26 18:09:12 +0530

    expresss content is added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const express=require("express")
+const express=require("express");
+const app=express();
+app.use("/static",express.Router("public"));
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.get('/',(req,res)=>{
+    res.render("todo.ejs");
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    })
+})
+app.listen(3000,()=>console.log("server up and running"));
